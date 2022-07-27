f48b82dbd6e9108b8e195efd53d344dd40280a1f
Stacy Lofton <lofton713@gmail.com>
2022-07-27 15:20:54 -0500

    my posts done

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Categories } from "../components/categories/Categories"
+import { MyPosts } from "../components/myPosts.js/MyPosts"
 import { PostForm } from "../components/postForm/PostForm"
 import { PostList } from "../components/PostList/PostList"
 import { Authorized } from "./Authorized"
@@ -19,6 +20,7 @@ export const ApplicationViews = ({ token, setToken }) => {
             <Route path="/" element={<PostList />}/>
             <Route path="create" element={<PostForm token={token} />}/>
             <Route path="categories" element={<Categories />} />
+            <Route path="myposts" element={<MyPosts token={token} />} />
           </>
         }
         
